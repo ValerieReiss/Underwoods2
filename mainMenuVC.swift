72bd8b7a2985e8e74dcece86f41bdba8645823b9
Valerie Reiss <127006059+ValerieReiss@users.noreply.github.com>
2023-08-21 15:37:05 +0200

    Add files via upload

---
+++ mainMenuVC.swift
@@ -0,0 +1,24 @@
+
+
+import UIKit
+
+class mainMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func unwindToMain(for unwindSegue: UIStoryboardSegue) {
+        //create an unwind function for players to restart the game and go back after looking the menu.
+        
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
